12da7f842cb846831437ad27572ead640ac58958
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2022-10-05 18:29:09 -0400

    lint changes

--- customer.py
+++ customer.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python3
+# import pdb  # debugging only
+
 import requests
 
-url_base = "https://us.api.insight.rapid7.com/ias/v1"
+URL_BASE = "https://us.api.insight.rapid7.com/ias/v1"
+REQUEST_TIMEOUT = 5
 
-##Customer class to handle all work done with both source and destination customers
+##Customer class to handle all work done with both source and destination
+# customers
 class Customer:
     def __init__(self, token):
-        self.url = url_base + "/"
+        self.url = URL_BASE + "/"
         self.payload = {}
         self.headers = {
             'Accept': 'application/json',
@@ -13,17 +18,18 @@ class Customer:
             'x-api-key': token
             }
 
-##Get apps from the original customer to be used to create apps on destination customer and copy configs over to
+##Get apps from the original customer to be used to create apps on 
+# destination customer and copy configs over to
 ##the corresponding new application
     def get_apps(self):
-<<<<<<< Updated upstream
-        self.url = url_base + "/apps"
+#<<<<<<< Updated upstream
+        self.url = URL_BASE + "/apps"
         self.payload = {}
-=======
+#=======
         print("Getting Apps")
-        url = f"{url_base}/apps"
+        url = f"{URL_BASE}/apps"
         try:
-            response = requests.get(url, headers=self.headers)
+            response = requests.get(url, headers=self.headers, timeout=REQUEST_TIMEOUT)
             response.raise_for_status()
         except requests.HTTPError as exception:
             return exception
@@ -36,13 +42,13 @@ class Customer:
         ## get sub data
     def get_configs(self, app_id):
         print("Getting Configs")
-        url = f"{url_base}/search"
+        url = f"{URL_BASE}/search"
         payload = {
             "query": f"scanconfig.app.id = '{app_id}'",
             "type": "SCAN_CONFIG"
             }
         try:
-            response = requests.post(url, headers=self.headers, json = payload)
+            response = requests.post(url, headers=self.headers, json = payload, timeout=REQUEST_TIMEOUT)
             response.raise_for_status()
         except requests.HTTPError as exception:
             return exception
@@ -54,40 +60,45 @@ class Customer:
             config['files'] = self.get_files(app_id)
             config['config_options'] = self.get_config_options(config['id'])
         return data
-    
+
     def get_files(self, app_id):
         print("Getting Files")
-        url = f"{url_base}/apps/{app_id}/files"
+        url = f"{URL_BASE}/apps/{app_id}/files"
         try:
-            response = requests.get(url, headers=self.headers)
+            response = requests.get(url, headers=self.headers, timeout=REQUEST_TIMEOUT)
             response.raise_for_status()
         except requests.HTTPError as exception:
             return exception
         data = response.json().get('data')
         return data
-           
-    def download_files(self, file_id):
+
+    def download_files(self):
+    # def download_files(self, file_id): # Dave's oroginal
         print("Downloading Files")
-        pass
-    
+        # pass
+
     def get_config_options(self, config_id):
         print("Getting Config Options:")
         #doesn't seem to be passing config ids
-        import pdb; pdb.set_trace()
-        url = f"{url_base}/scan-configs/{config_id}/options"
+        # pdb.set_trace()
+        url = f"{URL_BASE}/scan-configs/{config_id}/options"
         try:
-            response = requests.get(url, headers=self.headers)
+            response = requests.get(url, headers=self.headers, timeout=REQUEST_TIMEOUT)
             response.raise_for_status()
         except requests.HTTPError as exception:
             return exception
         data = response.json().get('data')
+        return data # Tim's addition
+        # need to do something with "data"
 
     def get_engine_groups(self):
         print("Getting Engine Groups")
-        url = f"{url_base}/engine-groups"
->>>>>>> Stashed changes
+        url = f"{URL_BASE}/engine-groups"
+        # need to do something with "url", may want to remove the "self."
+#>>>>>>> Stashed changes
         try:
-            response = requests.get(self.url, headers=self.headers, data = self.payload)
+            # response = requests.get(self.url, headers=self.headers, data = self.payload, timeout=REQUEST_TIMEOUT) # Dave's original
+            response = requests.get(url, headers=self.headers, data = self.payload, timeout=REQUEST_TIMEOUT)    # Tim's modification
             response.raise_for_status()
         except requests.HTTPError as exception:
             return exception
@@ -95,49 +106,51 @@ class Customer:
         #print("API Token-1: "+ str(self.token))
         return data
 
-    ##Create applications on the destination customer pulled from the source customer
+    ##Create applications on the destination customer pulled from the 
+    # source customer
     def create_apps(self, app_name, app_description):
-        self.url = url_base + "/apps"
+        self.url = URL_BASE + "/apps"
         self.payload = ('{"name": ' + str(app_name) + ', "description": ' + str(app_description) + '}')
-        response = requests.post(self.url, headers=self.headers, data = self.payload)
+        response = requests.post(self.url, headers=self.headers, data = self.payload, timeout=REQUEST_TIMEOUT)
         print(response.text.encode('utf8'))
         #print("API Token: "+ str(self.token))
 
-        location = response.headers.get('Location', None)
+        location = response.headers.get('Location', None, timeout=REQUEST_TIMEOUT)
         if location:
             print("successfully create: "+ app_name)
-        return 
+        # return
 
-"""
-##Get configs from the Original customer
-def get_configs():
-    pass
+# """
+# ##Get configs from the Original customer
+# def get_configs():
+#     pass
 
-##Get appid for apps created on the destination customer
-def get_new_app_id():
-    url_endpoint = "/apps"
-    url = url_base + url_endpoint
-    payload = {}
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'x-api-key': token_destination
-    }
-    if token_destination != token_source:
-        try:
-            response = requests.get(url, headers=headers, data = payload)
-            response.raise_for_status()
-        except requests.HTTPError as exception:
-            return exception
-        response = response.json()
-        name_list = []
-        for data in response['data']:
-            name_list.append(data['id'])
-            return name_list
-    else:
-        return("destination customer api key: "+ token_destination+ " matches source customer api key: "+ token_source+ ". These should be different. Please validate that you are using the correct API keys for both source and destination.")
+# ##Get appid for apps created on the destination customer
+# def get_new_app_id():
+#     url_endpoint = "/apps"
+#     url = URL_BASE + url_endpoint
+#     payload = {}
+#     headers = {
+#         'Accept': 'application/json',
+#         'Content-Type': 'application/json',
+#         'x-api-key': token_destination
+#     }
+#     if token_destination != token_source:
+#         try:
+#             response = requests.get(url, headers=headers, data = payload)
+#             response.raise_for_status()
+#         except requests.HTTPError as exception:
+#             return exception
+#         response = response.json()
+#         name_list = []
+#         for data in response['data']:
+#             name_list.append(data['id'])
+#             return name_list
+#     else:
+#         return("destination customer api key: "+ token_destination+ " matches source customer api key: "+ token_source+ ". These should be different. Please validate that you are using the correct API keys for both source and destination.")
 
-##Create configs on the destination customer based on the configs pulled from the original customer
-def create_configs():
-    pass
-"""
+# ##Create configs on the destination customer based on the configs pulled 
+# from the original customer
+# def create_configs():
+#     pass
+# """
--- migration_tool.py
+++ migration_tool.py
@@ -1,72 +1,69 @@
-import requests
-import json
+#!/usr/bin/python3
+
 from customer import Customer
 
+## Use Organization API Keys generated in Rapid7 Platform
+## User Input for required information
+TOKEN_ORIGINAL = input("Enter Original Customer API Key: ")
+TOKEN_DESTINATION = input("Enter Destination Customer API Key: ")
 
-##User Input for required information
-#token_original = input("Enter Original Customer API Key: ")
-#token_destination = input("Enter Destination Customer API Key: ")
+# TOKEN_ORIGINAL = ('REDACTED')       # source
+# TOKEN_DESTINATION = ('REDACTED')    # destination
 
-## Use predefined API Keys
-## use Razordemo -> "Applied Engineering - US" API Key for original customer 
-token_original = ('0f6352ba-107b-48ea-9f7d-a3d536eb4dd3')
-## Use Razordemo -> "Specialists Research" API Key for Destination
-token_destination = ('0c47f4a0-a3a5-411c-9e6b-149190618560')
-
-
-##Enumerate applications in the original customer
-enumerate_origin_apps = Customer(token_original)
-if token_original != "":
-#    enumerate_origin_apps.get_apps()
+## Enumerate applications in the original customer
+enumerate_origin_apps = Customer(TOKEN_ORIGINAL)
+if TOKEN_ORIGINAL != "":
+    ##enumerate_origin_apps.get_apps()
     app_enumeration = enumerate_origin_apps.get_apps()
 else:
     print("Original Customer API Key is missing.")
 
-#print(app_enumeration)
+# print(app_enumeration)
 
-create_app_in_cust_two = Customer(token_destination)
+create_app_in_cust_two = Customer(TOKEN_DESTINATION)
 for app in app_enumeration:
     create_app_in_cust_two.create_apps(app['name'], app.get('description', ''))
 
-"""
+# """
 
-##Enumerate app_list to create a list of apps with their appid, name and description
-##This will be used to feed back into the customer class to tell the create_apps function what apps to create
-##Is this the correct place to put this or should this work be done in the Customer class??
+# ## Enumerate app_list to create a list of apps with their appid, name and description
+# ## This will be used to feed back into the customer class to tell the
+# ## create_apps function what apps to create
+# ## Is this the correct place to put this or should this work be done in the Customer class??
 
-#app_name = [app_enumeration]
+# # app_name = [app_enumeration]
 
-#print(app_info(app_enumeration))
+# # print(app_info(app_enumeration))
 
-##Pseudocode for remaining requirements
+# ## Pseudocode for remaining requirements
 
-"""
-"""
-- pull app names from old customer -> app_list
-    - app_list - content
-        - app id
-        - app name
-        - app description
-- create apps in new customer based on app name(s) in app_list
+# """
+# """
+# - pull app names from old customer -> app_list
+#     - app_list - content
+#         - app id
+#         - app name
+#         - app description
+# - create apps in new customer based on app name(s) in app_list
 
-- pull users from old customer -> user_list
-    - user_list - content
-        - user name
-        - user email
+# - pull users from old customer -> user_list
+#     - user_list - content
+#         - user name
+#         - user email
 
-- iterate through apps to pull configs from each app -> config_list
-    - config_list - content
-        - config name
-        - config.xml
-        - files
+# - iterate through apps to pull configs from each app -> config_list
+#     - config_list - content
+#         - config name
+#         - config.xml
+#         - files
 
-- pull app ids and names from new customer -> app_list_new
-    - app_list_new - content
-        - app id
-        - app name
+# - pull app ids and names from new customer -> app_list_new
+#     - app_list_new - content
+#         - app id
+#         - app name
 
-- upload files from old customer to new customer
+# - upload files from old customer to new customer
 
-- create configs in new customer based on config name in config_list
+# - create configs in new customer based on config name in config_list
 
-"""
+# """
